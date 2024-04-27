da0c97231fdde42035e77cd7a329816e52027eb3
azarael01 <139633910+azarael01@users.noreply.github.com>
2024-04-27 01:05:57 +0000

    modified currency converter

--- django_project/views.py
+++ django_project/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 def home(request):
 #USING APIS
-  response = requests.get('https://api.github.com/events')
+  response = requests.get(' https://v6.exchangerate-api.com/v6/dac76accd7f2c8dfa2d7e9a1/latest/USD')
   data = response.json()
-  result = data[0]['repo']
+  result = data['conversion_rates']
   
   return render(request, 'templates/index.html', {'result': result})
